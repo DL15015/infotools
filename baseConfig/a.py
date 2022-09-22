36605a347e86e7541ca7b72b6f60b849742fd181
DL15015 <1478243841@qq.com>
2022-09-22 19:29:55 +0800

    update getpooldata

---
+++ baseConfig/a.py
@@ -0,0 +1,10 @@
+a = {
+    "data": {
+        "pools": [
+            {"name": "1"},
+            {"name": "2"}
+        ]
+    }
+}
+for i in a["data"]["pools"]:
+    print(i)
--- getData/getPoolData.py
+++ getData/getPoolData.py
@@ -22,9 +22,10 @@ def caculatePoolData():
         name = index["name"]
         tvl = float(index["tvlUSD"])
         _0hTvlList = index["_0hTvl"]
-        _24VolList = index["_0hTvl"]
-        _24_48VolList = index["_0hTvl"]
-        _7dVolList = index["_0hTvl"]
+        _24VolList = index["_24Vol"]
+        _24_48VolList = index["_24_48Vol"]
+        _7dVolList = index["_7dVol"]
+        _0hTvl = 0
 
         poolList["poolName"] = {
             "name": name,
@@ -39,11 +40,15 @@ def caculatePoolData():
             "FeeAPRChange": 0
         }
 
-        for analytics in _0hTvlList:
-            _0hTvl = float(analytics["tvlUSD"])
+        if _0hTvlList:
+            for analytics in _0hTvlList:
+                _0hTvl = float(analytics["tvlUSD"])
 
-            pool = poolList["poolName"]
-            pool["TVLChange"] = getChangeLate(tvl, _0hTvl)
+
+        pool = poolList["poolName"]
+        pool["TVLChange"] = getChangeLate(tvl, _0hTvl)
+        print(pool)
+
 
         _24Vol, _24Fee, _24FeeApr, _24Tvl = 0, 0, 0, 0
         for analytics in _24VolList:
@@ -52,8 +57,16 @@ def caculatePoolData():
             _24Vol += _24Vol
             _24Fee += _24Fee
 
-        if (tvl != 0) & (_24VolList != []):
-            _24FeeApr = (((_24Vol * _24Fee) / tvl) * 365 * 100)
+            # print(_24VolList)
+            # print(analytics)
+
+            # print(_24Vol)
+            # print(_24Fee)
+
+        # if (tvl != 0) & (_24VolList != []):
+        #     _24Tvl=float(["_24VolList"]["tvlUSD"])
+        #     _24FeeApr = (((_24Vol * _24Fee) / tvl) * 365 * 100)
+        #     pool["FeeAPR"]=_24FeeApr
 
         # for analytics in _24VolList:
         #     _24Vol = float(analytics["txVolumeUSD"])
